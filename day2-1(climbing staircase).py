ce01eac05205e80b8cb46dc741baa2e697fc6a06
pooji1475 <112894452+pooji1475@users.noreply.github.com>
2023-01-30 21:31:58 +0530

    Add files via upload

---
+++ day2-1(climbing staircase).py
@@ -0,0 +1,9 @@
+def fib(n):
+    if(n<=1):
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+def ways(w):
+    return fib(w+1)
+a=int(input("Enter no of stairs: "))
+print("Number of ways = ",ways(a))
